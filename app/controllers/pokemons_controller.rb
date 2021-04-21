80a5335a1cc9d9bf854cc28f1102417b3d49f02a
Clement Bisaillon <clement.bisaillon@hotmail.com>
2021-04-21 17:55:28 -0400

    Start pagination of pokemons on index

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,6 +1,42 @@
 class PokemonsController < ApplicationController
+  #
+  # Fetch the list of pokemons and return it paginated
   def index
-    @pokemons = Pokemon.all
-    render json: @pokemons
+    page = 1
+    per_page = 10
+
+    if params.has_key?(:page)
+      page = params[:page].to_i
+    end
+
+    if params.has_key?(:per_page)
+      per_page = params[:per_page].to_i
+    end
+
+    if page <= 0 or per_page <= 0
+      render :nothing => true, :status => :bad_request
+      return
+    end
+
+    @pokemons = Pokemon.limit(per_page).offset((page - 1) * per_page).all
+    render json: {
+      :page => page,
+      :per_page => per_page,
+      :has_next => true,
+      :has_prev => true,
+      :pokemons => @pokemons,
+    }
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def delete
+
   end
 end
